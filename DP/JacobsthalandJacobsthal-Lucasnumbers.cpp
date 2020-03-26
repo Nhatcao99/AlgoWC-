777ec88e00680d7afb3c97e337f1ea6890e39cfe
nhatcao <caominhnhat99@gmail.com>
2020-03-26 23:48:11 +0700

    blem blem

--- DP/JacobsthalandJacobsthal-Lucasnumbers.cpp
+++ DP/JacobsthalandJacobsthal-Lucasnumbers.cpp
@@ -5,7 +5,7 @@ int main(){
     cin >> n;
     int jab[n + 1] , jab_lucas[n + 1];
     jab[0] = 0 , jab[1] = 1 , jab_lucas[0] = 2 , jab_lucas[1] = 1;
-    for(int i = 2; i < n ; i ++){
+    for(int i = 2; i <= n ; i ++){
         jab[i] = jab[i - 1] + 2 * jab[i - 2];
         jab_lucas[i] = jab_lucas[i - 1] + 2 * jab[i - 2];
     }
